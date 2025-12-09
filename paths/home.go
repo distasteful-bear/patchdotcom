79f4617f80adce565f8cd63b14083ee17e8d8b33
James Metz <jamesmetz1231@gmail.com>
2025-12-08 19:03:04 -0500

    getting rolling.

--- paths/home.go
+++ paths/home.go
@@ -8,26 +8,12 @@ import (
 )
 
 func HomePage(c *gin.Context) {
-	Html404Page := []byte(`
-		<!doctype html>
-		<html>
-		    <head>
-		        <title>Patch Solutions</title>
-		        <script src="https://unpkg.com/htmx.org@1.9.10"></script>
-		    </head>
-		    <body>
-		        <h1>500 Page Unavailable</h1>
-		        <button hx-get="/ping" hx-swap="outerHTML">Test Ping</button>
-		    </body>
-		</html>
-		`)
-
 	filePath := "html/home.html"
 
 	fileContent, err := os.ReadFile(filePath)
 	if err != nil {
 		fmt.Println("Error reading file: ", filePath, err)
-		c.Data(500, "text/html; charset=utf-8", Html404Page)
+		c.JSON(500, gin.H{"error": "could not locate file"})
 		return
 	}
 
