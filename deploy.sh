4d9e7ee1499220cbe288f728245eaca069b3bbef
James Metz <jamesmetz1231@gmail.com>
2026-02-06 23:01:44 -0500

    changing region because region mapping is not allowed in us-east5

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ npx @tailwindcss/cli -i ./src/static/input.css -o ./src/static/output.css
 # upload, build and deploy application
 gcloud run deploy patchdotcom \
     --source . \
-    --region us-east5 \
+    --region us-east4 \
     --allow-unauthenticated \
     --port 8080
