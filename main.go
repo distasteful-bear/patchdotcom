68cad133dab8a0fc6c87c196a7632c0a09dc58a0
James Metz <jamesmetz1231@gmail.com>
2026-02-06 22:44:00 -0500

    finishing contact form implementation with sendgrid

--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import (
 	"net/http"
 	"os"
 
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+
 )
 
 type ContactForm struct {
@@ -36,9 +40,28 @@ func main() {
 
 		log.Printf("Contact form submission: %s %s <%s> - %s", form.FirstName, form.LastName, form.Email, form.Service)
 
-		// TODO: handle the submission (e.g. send email, store in DB)
+		from := mail.NewEmail("Patch Solutions Contact Form", "website@patch-solutions.com")
+		to := mail.NewEmail("Information", "info@patch-solutions.com")
 
-		c.JSON(http.StatusOK, gin.H{"message": "Thank you for reaching out! We'll be in touch soon."})
+		subject := "New Contact Form Submission"
+
+		plainTextContent := fmt.Sprintf("A new contact form submission from %s %s <%s> - %s.\nCompany %s\n Message:\n%s", form.FirstName, form.LastName, form.Email, form.Service, form.Company, form.Message)
+		htmlContent := fmt.Sprintf("A new contact form submission from %s %s <%s> - %s.\nCompany %s\n Message:\n%s", form.FirstName, form.LastName, form.Email, form.Service, form.Company, form.Message)
+		message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+
+		client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+
+		response, err := client.Send(message)
+
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "An error occurred while sending your message. Please contact us directly with the listed email address."})
+		} else {
+			fmt.Println(response.StatusCode)
+			fmt.Println(response.Body)
+			fmt.Println(response.Headers)
+			c.JSON(http.StatusOK, gin.H{"message": "Thank you for reaching out! We'll be in touch soon."})
+		}
 	})
 
 	port := os.Getenv("PORT")
